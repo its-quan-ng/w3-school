8545e5c92c1938ad741f0e57df52e97399da8d0c
Nguyen Quan <ngquan6704@gmail.com>
2025-08-24 22:27:01 +0700

    first commit

---
+++ w3_band/buyTicketModal.js
@@ -0,0 +1,22 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.js-modal-close')
+const modalContainer = document.querySelector('.js-modal-container')
+
+function showBuyTickets() {
+    modal.classList.add('open')
+}
+
+function hideBuyTickets() {
+    modal.classList.remove('open')
+}
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showBuyTickets)
+
+}
+
+modalClose.addEventListener('click', hideBuyTickets)
+modal.addEventListener('click', hideBuyTickets)
+modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
